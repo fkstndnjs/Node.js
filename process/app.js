f2cca871a207abd49ab6a41a32f93013c8f2a54a
fkstndnjs <fkstndnjs@naver.com>
2022-12-01 21:54:19 +0900

    fix

--- process/app.js
+++ process/app.js
@@ -1,1 +1,15 @@
 console.log("설치된 노드의 버전:", process.version);
+
+console.log("프로세서 아키텍쳐 정보:", process.arch);
+
+console.log("운영체제 플랫폼 정보:", process.platform);
+
+console.log("현재 프로세스의 아이디: ", process.pid);
+
+console.log("프로세스가 시작된 후 흐른 시간(단위는 초):", process.uptime());
+
+console.log("노드의 경로:", process.execPath);
+
+console.log("현재 프로세스가 실행되는 위치:", process.cwd());
+
+console.log("현재 cpu 사용량: ", process.cpuUsage());
