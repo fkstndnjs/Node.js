9f8367faa1af1c78f5d7c8f49f1823d7a1c86822
YuSeokHyun <fkstndnjs@naver.com>
2022-12-05 09:19:03 +0900

    fix

--- express/app.js
+++ express/app.js
@@ -3,29 +3,36 @@ import express from "express";
 // 서버 생성
 const app = express();
 
-// app.use
-// 지정한 경로 뒤에 어떤 경로가 와도 실행되는 미들웨어
-app.use("/", (req, res, next) => {
-  console.log("app.use()");
-  next();
-});
-
-// 경로를 아무것도 넣지 않으면 "/"로 설정된다.
-app.use((req, res, next) => {
-  console.log("app.use() 2");
-  next();
-});
-
 app.get("/", (req, res) => {
-  res.send("GET /");
+    res.send("GET /");
 });
 
 app.get("/test", (req, res) => {
-  res.send("GET /test");
+    res.send("GET /test");
 });
 
-app.get("/test/test", (req, res) => {
-  res.send("GET /test/test");
+// e
+app.get("/error", (req, res, next) => {
+    next(new Error("Error!"));
+});
+
+app.get("/error2", (req, res, next) => {
+    try {
+        throw new Error("Error 2!");
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 요청이 들어온 url을 담당하는 미들웨어가 없을 때
+app.use((req, res, next) => {
+    res.status(404).send("NOT FOUND");
+});
+
+// 에러 핸들러
+app.use((error, req, res, next) => {
+    console.log(error);
+    res.send("ERROR");
 });
 
 // 8080포트로 서버 열기
