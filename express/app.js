a833da1795f971d904337570b49da83cb2154c3f
YuSeokHyun <fkstndnjs@naver.com>
2022-12-05 12:01:48 +0900

    fix

--- express/app.js
+++ express/app.js
@@ -1,10 +1,12 @@
 import express from "express";
-import router from "./app-router";
+import userRouter from "./app-router";
 
 // 서버 생성
 const app = express();
 
-app.use("/user", router);
+// /user 경로로 들어오는 모든 요청은 userRouter에서 처리한다
+// app.js 파일에 모든 코드가 있는 것보다 훨씬 보기 편하고 유지 보수가 쉽다
+app.use("/user", userRouter);
 
 // 8000포트로 서버 열기
 app.listen(8000);
