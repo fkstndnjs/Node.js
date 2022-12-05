9a90a0e7f9e346d7417774087aba8c232a12d114
YuSeokHyun <fkstndnjs@naver.com>
2022-12-05 11:58:52 +0900

    fix

--- express/app.js
+++ express/app.js
@@ -1,9 +1,10 @@
 import express from "express";
+import router from "./app-router";
 
 // 서버 생성
 const app = express();
 
-app.use("/user");
+app.use("/user", router);
 
 // 8000포트로 서버 열기
 app.listen(8000);
