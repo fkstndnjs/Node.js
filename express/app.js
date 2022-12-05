242bcbd71f7c44f4bf9ffd4bcf33288e84a6c5c7
YuSeokHyun <fkstndnjs@naver.com>
2022-12-05 13:55:38 +0900

    fix

--- express/app.js
+++ express/app.js
@@ -1,14 +1,11 @@
 import express from "express";
-// "type": "module"에서 파일을 import할 때는 확장자까지 붙여주어야 한다
-// 안하면 [ERR_MODULE_NOT_FOUND] 에러가 발생한다
-import userRouter from "./app-router.js ";
 
 // 서버 생성
 const app = express();
 
-// /user 경로로 들어오는 모든 요청은 userRouter에서 처리한다
-// app.js 파일에 모든 코드가 있는 것보다 훨씬 보기 편하고 유지 보수가 쉽다
-app.use("/user", userRouter);
+app.get("/", (req, res) => {
+    res.send("HOME");
+});
 
 // 8000포트로 서버 열기
 app.listen(8000);
